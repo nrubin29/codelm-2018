9bc9d4bf935407efd9fddd032c212d0611173c8b
nrubin29 <nrubin29@gmail.com>
2018-02-03 17:54:55 -0500

    Added submissions to problem page.

--- src/frontend/app/views/dashboard/problem/problem.component.ts
+++ src/frontend/app/views/dashboard/problem/problem.component.ts
@@ -4,8 +4,9 @@ import 'rxjs/add/operator/switchMap';
 import 'codemirror/mode/python/python';
 import { ProblemModel } from '../../../../../common/models/problem.model';
 import { ProblemService } from '../../../services/problem.service';
-import { Observable } from 'rxjs/Observable';
 import { CodemirrorComponent } from 'ng2-codemirror';
+import { SubmissionModel } from '../../../../../common/models/submission.model';
+import { TeamService } from '../../../services/team.service';
 
 @Component({
   selector: 'app-problem',
@@ -14,14 +15,21 @@ import { CodemirrorComponent } from 'ng2-codemirror';
 })
 export class ProblemComponent implements OnInit, AfterViewInit {
   problem: ProblemModel;
+  submissions: SubmissionModel[] = [];
   @ViewChildren(CodemirrorComponent) codeMirrors: QueryList<CodemirrorComponent>;
 
-  constructor(private problemService: ProblemService, private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(private problemService: ProblemService, private teamService: TeamService, private router: Router, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activatedRoute.params.subscribe(params => {
       this.problem = null;
-      this.problemService.getProblem(params['id']).then(problem => this.problem = problem).catch(console.log);
+      this.submissions = [];
+
+      this.problemService.getProblem(params['id']).then(problem => {
+        this.problem = problem;
+        console.log(this.teamService.team.submissions);
+        this.submissions = this.teamService.team.submissions.filter(submission => submission.problem._id === problem._id);
+      }).catch(console.log);
     });
   }
 

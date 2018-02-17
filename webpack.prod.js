fe49977ad07e6fb4705c7ea0a8678089a7292ee9
nrubin29 <nrubin29@gmail.com>
2018-02-16 21:02:54 -0500

    Added multiple webpack configurations. Set index.html to not cache.

---
+++ webpack.prod.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+    devtool: 'source-map',
+    plugins: [
+        new UglifyJSPlugin({sourceMap: true}),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
+});
